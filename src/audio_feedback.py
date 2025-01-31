f2eef8ada87aefb637b0cf0562ad65d5eabcbf7f
E-v7 <evanapogue@gmail.com>
2025-01-31 16:23:31 -0500

    Created function to handle playing audio file

--- src/audio_feedback.py
+++ src/audio_feedback.py
@@ -2,6 +2,14 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import time, mixer
 
+mixer.init()
+
+def playAudioFile(audioFile: str) -> mixer.Channel:
+    sound = mixer.Sound(audioFile)
+    channel = sound.play()
+
+    return channel
+
 if __name__ == "__main__":
     mixer.init()
     test_sound = mixer.Sound("data/audio/test.wav")
