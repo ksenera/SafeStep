9fb4e65079b4960f74ed08d68dc7821bd32088a4
taudet <thomas.audet@live.com>
2025-02-03 21:01:10 -0500

    addead some basic functions for controlling the vibration motors will need to be changed to fit our needs

---
+++ src/vibration_feedback.py
@@ -0,0 +1,108 @@
+"""
+* File: vibration_feedback.py
+* Project: DMDforVI
+* First version: 2025 - 01 - 30
+* Description: This files holds the necessary functionality to send signals to the vibration device
+"""
+from gpiozero import OutputDevice
+import time
+
+gpio_pin_right = 18
+gpio_pin_left = 17
+gpio_pin_middle = 16
+
+"""
+* Function    : startup_pulse
+* Description : Send the pulse signal for startup feedback
+* Parameters  : gpio_pin : holds the gpio pin where the signal is sent which is connected to the motor
+* Returns     : NULL
+"""
+def startup_pulse(gpio_pin):
+    device = OutputDevice(gpio_pin)
+    i = 0
+
+    #sends a pulse of 1 second on and 1 second off 5 times can be changed depending on th pulse length we want
+    while i < 5:
+        device.on()
+        time.sleep(1)
+        device.off()
+        time.sleep(1)
+        i += 1
+
+"""
+* Function    : sleep_pulse
+* Description : Send the pulse signal for sleep feedback
+* Parameters  : gpio_pin : holds the gpio pin where the signal is sent which is connected to the motor
+* Returns     : NULL
+"""
+
+def sleep_pulse(gpio_pin):
+    device = OutputDevice(gpio_pin)
+
+    device.on()
+
+    time.sleep(5)
+
+    device.off()
+
+
+"""
+* Function    : error_pulse
+* Description : Send the pulse signal for error feedback
+* Parameters  : gpio_pin : holds the gpio pin where the signal is sent which is connected to the motor
+* Returns     : NULL
+"""
+
+def error_pulse(gpio_pin):
+    device = OutputDevice(gpio_pin)
+    i = 0
+
+    # sends a pulse of 1 second on and 3 second off 5 times can be changed depending on th pulse length we want
+    while int < 5:
+        device.on()
+        time.sleep(1)
+        device.off()
+        time.sleep(3)
+
+
+"""
+* Function    : two_vibrator_pulse
+* Description : Send the pulse signal to two different motors
+* Parameters  : gpio_pin1 : holds one of the pins the signal is sent too, gpio_pin2 : holds one of the pins the signal is sent too
+* Returns     : NULL
+"""
+
+def two_vibrator_pulse(gpio_pin1 , gpio_pin2):
+    device1 = OutputDevice(gpio_pin1)
+    device2 = OutputDevice(gpio_pin2)
+
+    device1.on()
+    device2.on()
+
+    time.sleep(2)
+
+    device1.off()
+    device2.off()
+
+
+"""
+* Function    : three_vibrator_pulse
+* Description : Send the pulse signal to 3 different motors
+* Parameters  : gpio_pin1 : holds one of the pins the signal is sent too, gpio_pin2 : holds one of the pins the signal is sent too, gpio_pin3 : holds one of the pins the signal is sent too
+* Returns     : NULL
+"""
+
+def three_vibrator_pulse(gpio_pin1 , gpio_pin2, gpio_pin3):
+    device1 = OutputDevice(gpio_pin1)
+    device2 = OutputDevice(gpio_pin2)
+    device3 = OutputDevice(gpio_pin3)
+
+    device1.on()
+    device2.on()
+    device3.on()
+
+    time.sleep(2)
+
+    device1.off()
+    device2.off()
+    device3.off()
