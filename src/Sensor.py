c21c909aeb8a0196c1301b657b45f1259f3d2ad1
E-v7 <evanapogue@gmail.com>
2025-02-05 15:41:44 -0500

    Added support for multiplexer use

--- src/Sensor.py
+++ src/Sensor.py
@@ -9,8 +9,11 @@ I2C_BUS = 1
     Parameters: sensor_address - The address of the sensor (ex. 0x29)
                 ranging_mode - The ranging mode to start in
 """
-def initialize_sensor(sensor_address: int, ranging_mode: VL53L1X.VL53L1xDistanceMode) -> VL53L1X.VL53L1X:
-    sensor = VL53L1X.VL53L1X(i2c_bus=I2C_BUS, i2c_address=sensor_address)
+def initialize_sensor(sensor_address: int, 
+                      ranging_mode: VL53L1X.VL53L1xDistanceMode = VL53L1X.VL53L1xDistanceMode.SHORT, 
+                      multiplexer_address: int = 0, 
+                      multiplexer_channel: int = 255) -> VL53L1X.VL53L1X:
+    sensor = VL53L1X.VL53L1X(i2c_bus=I2C_BUS, i2c_address=sensor_address, tca9548a_addr=multiplexer_address, tca9548a_num=multiplexer_channel)
     sensor.open()
     sensor.start_ranging(ranging_mode)
 
