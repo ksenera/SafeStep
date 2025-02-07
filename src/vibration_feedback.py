f69aebd5b5727ce9360e71e04ed46ecf0b4c63bb
taudet <thomas.audet@live.com>
2025-02-06 20:00:27 -0500

    added suggested time function parameter and modified some of the pulses to include three motors

--- src/vibration_feedback.py
+++ src/vibration_feedback.py
@@ -17,15 +17,21 @@ gpio_pin_middle = 16
 * Parameters  : gpio_pin : holds the gpio pin where the signal is sent which is connected to the motor
 * Returns     : NULL
 """
-def startup_pulse(gpio_pin):
-    device = OutputDevice(gpio_pin)
+def startup_pulse(gpio_pin1, gpio_pin2, gpio_pin3):
+    device1 = OutputDevice(gpio_pin1)
+    device2 = OutputDevice(gpio_pin2)
+    device3 = OutputDevice(gpio_pin3)
     i = 0
 
     #sends a pulse of 1 second on and 1 second off 5 times can be changed depending on th pulse length we want
     while i < 5:
-        device.on()
+        device1.on()
+        device2.on()
+        device3.on()
         time.sleep(1)
-        device.off()
+        device1.off()
+        device2.off()
+        device3.off()
         time.sleep(1)
         i += 1
 
@@ -36,14 +42,20 @@ def startup_pulse(gpio_pin):
 * Returns     : NULL
 """
 
-def sleep_pulse(gpio_pin):
-    device = OutputDevice(gpio_pin)
+def sleep_pulse(gpio_pin1, gpio_pin2, gpio_pin3):
+    device1 = OutputDevice(gpio_pin1)
+    device2 = OutputDevice(gpio_pin2)
+    device3 = OutputDevice(gpio_pin3)
 
-    device.on()
+    device1.on()
+    device2.on()
+    device3.on()
 
     time.sleep(5)
 
-    device.off()
+    device1.off()
+    device2.off()
+    device3.off()
 
 
 """
@@ -53,33 +65,56 @@ def sleep_pulse(gpio_pin):
 * Returns     : NULL
 """
 
-def error_pulse(gpio_pin):
-    device = OutputDevice(gpio_pin)
+def error_pulse(gpio_pin1, gpio_pin2, gpio_pin3):
+    device1 = OutputDevice(gpio_pin1)
+    device2 = OutputDevice(gpio_pin2)
+    device3 = OutputDevice(gpio_pin3)
     i = 0
 
     # sends a pulse of 1 second on and 3 second off 5 times can be changed depending on th pulse length we want
     while int < 5:
-        device.on()
+        device1.on()
+        device2.on()
+        device3.on()
         time.sleep(1)
-        device.off()
+        device1.off()
+        device2.off()
+        device3.off()
         time.sleep(3)
+
+
+"""
+* Function    : one_vibrator_pulse
+* Description : Send the pulse signal to one motor
+* Parameters  : gpio_pin1 : holds one of the pins the signal is sent to, timespan : allows you to set the time is seconds for the delay
+* Returns     : NULL
+"""
+
+def one_vibrator_pulse (gpio_pin1, timespan):
+    device1 = OutputDevice(gpio_pin1)
+
+    device1.on()
+
+    time.sleep(timespan)
+
+    device1.off()
 
 
 """
 * Function    : two_vibrator_pulse
 * Description : Send the pulse signal to two different motors
-* Parameters  : gpio_pin1 : holds one of the pins the signal is sent too, gpio_pin2 : holds one of the pins the signal is sent too
+* Parameters  : gpio_pin1 : holds one of the pins the signal is sent to, gpio_pin2 : holds one of the pins the signal is sent to, timespan : allows you to set the time is seconds for the delay
 * Returns     : NULL
 """
 
-def two_vibrator_pulse(gpio_pin1 , gpio_pin2):
+def two_vibrator_pulse(gpio_pin1 , gpio_pin2, timespan):
     device1 = OutputDevice(gpio_pin1)
     device2 = OutputDevice(gpio_pin2)
 
     device1.on()
     device2.on()
 
-    time.sleep(2)
+    time.sleep(timespan)
 
     device1.off()
     device2.off()
@@ -88,11 +123,11 @@ def two_vibrator_pulse(gpio_pin1 , gpio_pin2):
 """
 * Function    : three_vibrator_pulse
 * Description : Send the pulse signal to 3 different motors
-* Parameters  : gpio_pin1 : holds one of the pins the signal is sent too, gpio_pin2 : holds one of the pins the signal is sent too, gpio_pin3 : holds one of the pins the signal is sent too
+* Parameters  : gpio_pin1 : holds one of the pins the signal is sent to, gpio_pin2 : holds one of the pins the signal is sent too, gpio_pin3 : holds one of the pins the signal is sent too, allows you to set the time is seconds for the delay
 * Returns     : NULL
 """
 
-def three_vibrator_pulse(gpio_pin1 , gpio_pin2, gpio_pin3):
+def three_vibrator_pulse(gpio_pin1 , gpio_pin2, gpio_pin3, timespan):
     device1 = OutputDevice(gpio_pin1)
     device2 = OutputDevice(gpio_pin2)
     device3 = OutputDevice(gpio_pin3)
@@ -101,7 +136,7 @@ def three_vibrator_pulse(gpio_pin1 , gpio_pin2, gpio_pin3):
     device2.on()
     device3.on()
 
-    time.sleep(2)
+    time.sleep(timespan)
 
     device1.off()
     device2.off()
