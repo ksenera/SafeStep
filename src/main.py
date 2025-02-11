9c3282643efda210f47fc37c1f314a1dc2e2defe
E-v7 <evanapogue@gmail.com>
2025-02-10 19:27:00 -0500

    Created function to handle turning on/off vibrational pulsing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from Sensor import initialize_all_sensors, shutdown_all_sensors
 from vibration_feedback import timed_vibrator_pulse
+import threading
+from time import sleep
 
 # 3 meters
 OUTER_RANGE_MM = 3000
@@ -11,6 +13,9 @@ VIBRATOR_PINS = [
     2,
     3
 ]
+
+
+task_dict = {}
 
 
 """
@@ -30,6 +35,43 @@ def determine_vibrator(sensor_index: int, sensor_count: int) -> int:
     return VIBRATOR_PINS[vibrator_index]
 
 
+def vibrator_loop(vibrator_gpio: int, timespan: int) -> None:
+    while True:
+        timed_vibrator_pulse(timespan=timespan, gpio_pin1=vibrator_gpio)
+        sleep(timespan)
+
+
+class ThreadInfo:
+    def __init__(self, task, distance):
+        self.task = task
+        self.distance = distance
+
+
+def determine_delay(distance: int) -> int | None:
+    pass
+
+
+def handle_vibrational_pulsing(vibrator_gpio, distance):
+    info = task_dict.get(vibrator_gpio)
+    new_delay = determine_delay(distance)
+
+    # Check for prexisting task
+    if info is not None:
+        # Determine if new distance requires different vibrational timing
+        if determine_delay(info.distance) != new_delay:
+            info.task.cancel()
+            if new_delay is not None:
+                info.task = threading.Thread(target=vibrator_loop, args=[vibrator_gpio, new_delay])
+            else:
+                task_dict.pop(vibrator_gpio)
+    else:
+        if new_delay is not None:
+            # Create new task
+            task = threading.Thread(target=vibrator_loop, args=[vibrator_gpio, new_delay])
+            new_info = ThreadInfo(task, distance)
+            task_dict[vibrator_gpio] = new_info
+
+
 if __name__ == "__main__":
     # Project flow
 
@@ -47,6 +89,5 @@ if __name__ == "__main__":
                 vibrator_gpio = determine_vibrator(index, len(sensor_list))
 
                 # Determine how long a vibrator should be pulsed for
-                timespan = 1
-                timed_vibrator_pulse(timespan, vibrator_gpio)
+                handle_vibrational_pulsing(vibrator_gpio, distance)
 
