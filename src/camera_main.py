7c0ddc629a67bd7c01b981a0ac9fb6ff5270e328
taudet <thomas.audet@live.com>
2025-03-30 17:35:14 -0400

    deleted term

--- src/camera_main.py
+++ src/camera_main.py
@@ -2,17 +2,12 @@ import signal
 from thread_workers import THREAD_KILL, handleCamera
 from Camera import camera_init, detection_model_init, close_camera
 
-
-
 def cleanup_processes(signum, frame):
     THREAD_KILL.set()
     close_camera()
     
-
-
 def main():
     signal.signal(signal.SIGINT, cleanup_processes)
-    signal.signal(signal.SIGTERM, cleanup_processes)
 
     try: 
         camera_init()
